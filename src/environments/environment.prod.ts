2010c472ff8e5f2e53c0f7d9dc7c1ef64f229dab
Kendrick <kendrick.wh@outlook.com>
2020-01-18 17:16:00 +0800

    update repositories

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  links: {
+    github: 'https://github.com/KendrickAng',
+    linkedin: 'https://www.linkedin.com/in/kendrickang/',
+    instagram: 'https://www.instagram.com/kenn_sir/'
+  },
+  email: 'kendrick.wh@outlook.com',
+  contact: '+65 83606608'
+};
